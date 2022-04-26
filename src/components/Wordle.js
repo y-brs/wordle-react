81a5904a298c3345f55a035729a7abc54aafa7d9
boris ya <boris.yarmoulnik@gmail.com>
2022-04-27 00:43:44 +0300

    feat: adding new guess

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,13 +2,17 @@ import { useEffect } from "react"
 import useWordle from "../hooks/useWordle"
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyUp } = useWordle(solution)
+  const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyUp)
 
     return () => window.removeEventListener("keyup", handleKeyUp)
   }, [handleKeyUp])
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
 
   return (
     <>
