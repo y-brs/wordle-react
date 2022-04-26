ee7779e2336a0a346451ebc4bb920672a9e56b9d
boris ya <boris.yarmoulnik@gmail.com>
2022-04-27 02:06:37 +0300

    feat: showing past and current guesses with animation

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,11 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
       {guesses.map((g, i) => {
-        return <Row key={i} />
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />
+        }
+
+        return <Row key={i} guess={g} />
       })}
     </div>
   )
